50294ead0dc3d134d1157b627eddcc281e345da1
Hanmant Pawar <103906859+hanmanttpawar@users.noreply.github.com>
2022-10-02 01:31:56 +0530

    azentevent

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+// app () -this is commponent and jsx return
+function App() {
+  return (
+      <div className="App"> {/* class name object property*/}
+      <Navbar/>
+      <div className='title'>Explore Our Events</div>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
